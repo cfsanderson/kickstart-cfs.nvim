a2e950cf63331105ee02b43d07e339ed25881631
Caleb Frost Sanderson <caleb@calebsanderson.com>
2024-03-23 02:19:18 -0400

    adds link and comment for ater/ftplugin/markdown.lua

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,3 +1,6 @@
+-- automatically set wrap and linebreak for all markdown files
+-- https://www.reddit.com/r/neovim/comments/ujd7o1/only_set_wrap_for_specific_filetypes_with_lua/
+
 vim.api.nvim_create_autocmd('BufEnter', {
   pattern = { '*.md' },
   -- group = group,
