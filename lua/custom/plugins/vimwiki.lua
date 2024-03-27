1510e2de5bbd835d2bba64c3559b589088c142a5
Caleb Frost Sanderson <caleb.sanderson@spatialnetworks.com>
2024-03-27 16:04:53 -0400

    updates vimwiki.lua to include HTML support but doen't work that well - revert?

--- lua/custom/plugins/vimwiki.lua
+++ lua/custom/plugins/vimwiki.lua
@@ -1,12 +1,15 @@
 vim.g.vimwiki_list = {
-	{
-		path = '$HOME/Projects/.wiki',
-		syntax = 'markdown',
-		ext = '.md',
-		links_space_char = '_',
-		custom_wiki2html = 'vimwiki_markdown',
-		html_filename_parameterization = 1
-	} }
+  {
+    path = '$HOME/Projects/wiki',
+    path_html = 'wiki_html_location',
+    syntax = 'markdown',
+    ext = '.md',
+    links_space_char = '_',
+    custom_wiki2html = '~/Projects/wiki/wiki2html.sh',
+    -- html_filename_parameterization = 1
+    -- https://gist.github.com/enpassant/0496e3db19e32e110edca03647c36541
+  },
+}
 
 vim.g.taskwiki_markup_syntax = 'markdown'
 vim.g.markdown_folding = 1
@@ -18,5 +21,5 @@ vim.g.vimwiki_markdown_link_ext = 1
 vim.g.vimwiki_auto_header = 1
 
 return {
-	"vimwiki/vimwiki",
+  'vimwiki/vimwiki',
 }
