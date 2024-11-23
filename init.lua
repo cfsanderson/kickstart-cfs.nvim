65f938e22de68732a6ef380607bf19b10003e115
Caleb Frost Sanderson <me@calebsanderson.com>
2024-11-22 22:16:33 -0500

    switches to gruvbox material hard dark instead of my personal fork

--- init.lua
+++ init.lua
@@ -841,18 +841,19 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'folke/tokyonight.nvim',
-    'cfsanderson/cfs-gruvbox-material',
+    -- 'cfsanderson/cfs-gruvbox-material',
+    'sainnhe/gruvbox-material',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      vim.g.gruvbox_material_foreground = 'material'
-      vim.g.gruvbox_material_background = 'hard'
-      vim.cmd.colorscheme 'gruvbox-material'
-    end,
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd [[colorscheme gruvbox-material]]
+      vim.o.background = 'dark'
+      vim.g.gruvbox_material_foreground = 'material'
+      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_enable_italic_comment = 1
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
