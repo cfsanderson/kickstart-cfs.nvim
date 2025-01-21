f6918b20e80159436ec9c910c36316fa55aa624a
Caleb Frost Sanderson <caleb.sanderson@spatialnetworks.com>
2025-01-21 17:12:40 -0500

    updates obsidian config

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -2,14 +2,14 @@ return {
   'epwalsh/obsidian.nvim',
   version = '*', -- recommended, use latest release instead of latest commit
   lazy = true,
-  ft = 'markdown',
+  -- ft = 'markdown',
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   event = {
     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
     -- refer to `:h file-pattern` for more examples
-    -- 'BufReadPre $HOME/Projects/second-brain/*.md',
-    -- 'BufNewFile $HOME/Projects/second-brain/*.md',
+    'BufReadPre $HOME/Projects/second-brain/*.md',
+    'BufNewFile $HOME/Projects/second-brain/*.md',
   },
   dependencies = {
     -- Required.
@@ -44,18 +44,18 @@ return {
         },
         -- could add toggle for check boxes with leader od
       },
-      note_frontmatter_func = function(note)
-        -- This is equivalent to the default frontmatter function.
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags, area = '', project = '' }
-        -- 'note.metadata' contains any manyally added fields in the frontmatter.
-        -- So here we just make sure those fields are kept in the frontmatter.
-        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-          for k, v in pairs(note.metadata) do
-            out[k] = v
-          end
-        end
-        return out
-      end,
+      -- note_frontmatter_func = function(note)
+      --   -- This is equivalent to the default frontmatter function.
+      --   local out = { id = note.id, aliases = note.aliases, tags = note.tags, area = '', project = '' }
+      --   -- 'note.metadata' contains any manyally added fields in the frontmatter.
+      --   -- So here we just make sure those fields are kept in the frontmatter.
+      --   if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+      --     for k, v in pairs(note.metadata) do
+      --       out[k] = v
+      --     end
+      --   end
+      --   return out
+      -- end,
 
       templates = {
         subdir = 'Templates',
